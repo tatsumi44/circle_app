fec880bead3bc5995297315b2b4f42cc6f00fb1f
tatsumi kentaro <tatsuken4290@gmail.com>
2018-05-17 21:18:53 +0900

    円上に任意のボタン追加

--- circle_app/ViewController.swift
+++ circle_app/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    var num : Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +23,17 @@ class ViewController: UIViewController {
         
         let path: UIBezierPath = UIBezierPath();
         path.move(to: CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2))
-        path.addArc(withCenter: CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2), radius: 100, startAngle: start, endAngle: end, clockwise: true) // 円弧
+        path.addArc(withCenter: CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2), radius: 180, startAngle: start, endAngle: end, clockwise: true) // 円弧
         
         let layer = CAShapeLayer()
         layer.fillColor = UIColor.orange.cgColor
+        layer.borderColor = UIColor.black.cgColor
+        layer.lineWidth = 3.0
         layer.path = path.cgPath
         
         self.view.layer.addSublayer(layer)
+
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +41,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func decide(_ sender: Any) {
+        if textField.text != nil{
+            num = Int(textField.text!)
+            for i in 1 ... num{
+                let num_arc : Double = Double(360 / num)
+                let θ = Double.pi / Double(180) * Double(num_arc * Double(i))
+                let button = UIButton()
+                button.tag = num
+                button.frame = CGRect(x: CGFloat(Double(self.view.frame.width/2 - 15) + 180 * cos(θ)), y: CGFloat(Double(self.view.frame.height/2 - 15) + 180 * sin(θ)), width: 30, height: 30)
+                print("θ\(θ)")
+                print("X:\(Double(self.view.frame.width/2) + 20 * cos(θ)),Y:\(Double(self.view.frame.height/2) + 20 * sin(θ))")
+                button.backgroundColor = UIColor.blue
+                button.addTarget(self, action: #selector(self.btn_click), for: .touchUpInside)
+                view.addSubview(button)
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        textField.text = ""
+        for v in view.subviews{
+            if let v = v as? UIButton, v.tag >= 1 && v.tag <= num {
+                v.removeFromSuperview()
+            }
+        }
+    }
+    
+    @objc func btn_click() {
+        print(num)
+    }
+    
 }
 
